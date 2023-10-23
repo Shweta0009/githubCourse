8fc6cbe47a05a86302fa7225d81bee668c4c5c03
Shweta0009 <shwetarai052004@gmail.com>
2023-10-23 17:08:50 +0530

    add and modified

--- src/test/homePageTest.java
+++ src/test/homePageTest.java
@@ -4,6 +4,7 @@ public class homePageTest {
 
 	public static void main(String[] args) {
 		System.out.println("home page ");
+		System.out.println("look and book");
 	}
 
 }
--- src/test/myTest.java
+++ src/test/myTest.java
@@ -5,6 +5,7 @@ public class myTest {
 	public static void main(String[] args) {
 int i = 70;
 String City= "lucknow";
+String name = "Shweta";
 	}
 
 }
