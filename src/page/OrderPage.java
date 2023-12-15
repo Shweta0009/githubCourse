b0e92bac6a2062ed5024be69f855a0074f798146
Shweta0009 <shwetarai052004@gmail.com>
2023-12-15 22:01:22 +0530

    modifiedOrderPage

--- src/page/OrderPage.java
+++ src/page/OrderPage.java
@@ -3,8 +3,8 @@ package page;
 public class OrderPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("this is order page ");
+		
 	}
 
 }
